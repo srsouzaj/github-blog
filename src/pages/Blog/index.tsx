3ac59c09bacb995a278893ab7e4567e964a6cc00
Jorge de Souza J Junior <jorgedesouza@recrutae.com.br>
2022-10-07 15:16:53 -0300

    feat: logo

--- src/pages/Blog/index.tsx
+++ src/pages/Blog/index.tsx
@@ -1,5 +1,11 @@
+import Logo from "../../assets/logo/Logo.icon"
+
 export const BlogScreen = () => {
-    return(
+    return (
+      <div>
+        <Logo />
+
         <h1>Jorge de Souza</h1>
-    )
+      </div>
+    );
 }

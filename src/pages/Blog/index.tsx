100d4bdb4722fd3b3e683c344b0c00aa6d871dce
Jorge de Souza J Junior <jorgedesouza@recrutae.com.br>
2022-10-07 16:59:27 -0300

    feat: inputsearch

--- src/pages/Blog/index.tsx
+++ src/pages/Blog/index.tsx
@@ -1,10 +1,12 @@
 import Logo from "../../assets/logo/Logo.icon"
 import { ProfileSection } from "./components/Profile";
+import { SearchInput } from "./components/SearchInput";
 
 export const BlogScreen = () => {
     return (
       <>
         <ProfileSection />
+        <SearchInput />
       </>
     );
 }

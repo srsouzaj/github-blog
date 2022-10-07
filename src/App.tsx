fa6ffc7e0eb62b0c576620330b8f2c728a0541e1
Jorge de Souza J Junior <jorgedesouza@recrutae.com.br>
2022-10-07 15:05:32 -0300

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Logo from "./assets/logo/Logo.icon"
+
+function App() {
+
+  return (
+<div>
+  <Logo />
+   </div>  )
+}
+
+export default App
---
+++ src/assets/logo/Logo.icon.tsx
@@ -0,0 +1,26 @@
+function Logo() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="46"
+      height="40"
+      fill="none"
+      viewBox="0 0 46 40"
+    >
+      <g clipPath="url(#clip0_406_120)">
+        <path
+          stroke="#3294F8"
+          strokeWidth="1.5"
+          d="M19.235 18.765h0a1.744 1.744 0 010 2.47h0l-14.997 15a1.75 1.75 0 01-2.475 0 1.745 1.745 0 010-2.47h0L14.99 20.53l.53-.53-.53-.53L1.763 6.238s0 0 0 0a1.75 1.75 0 010-2.475s0 0 0 0 0 0 0 0a1.751 1.751 0 012.475 0l14.997 15.002zM43 33.25c.969 0 1.75.781 1.75 1.75s-.781 1.75-1.75 1.75H20.5c-.969 0-1.75-.781-1.75-1.75s.781-1.75 1.75-1.75H43z"
+        ></path>
+      </g>
+      <defs>
+        <clipPath id="clip0_406_120">
+          <path fill="#fff" d="M0 0H45V40H0z" transform="translate(.5)"></path>
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
+
+export default Logo;

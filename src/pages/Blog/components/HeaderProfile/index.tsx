33d9beef68c7278e20846c634c0e629fa6b82a92
Jorge de Souza J Junior <jorgedesouza@recrutae.com.br>
2022-10-08 13:31:18 -0300

    feat: profileData

--- src/pages/Blog/components/HeaderProfile/index.tsx
+++ src/pages/Blog/components/HeaderProfile/index.tsx
@@ -3,31 +3,64 @@ import { ProfileContainer, ProfileDetails, ProfilePicture } from "./styles"
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
 import { faBuilding, faUserGroup } from "@fortawesome/free-solid-svg-icons";
+import React from "react";
+import { api } from "../../../../services/api/base.api";
+
+const username = import.meta.env.VITE_GITHUB_USERNAME;
+interface ProfileInterface {
+  name: string;
+  login: string;
+  avatar_url: string;
+  bio: string;
+  html_url: string;
+  company?: string;
+  followers: number;
+}
 
 export const HeaderProfileSection = () => {
+const [profileData, setProfileData] = React.useState<ProfileInterface>({} as ProfileInterface);
+const [isLoading, setIsLoading] = React.useState<boolean>(true)
+
+const getProfileData = React.useCallback(async () => {
+  try {
+    setIsLoading(true);
+    const response = await api.get(`/users/${username}`);
+
+    setProfileData(response.data);
+  } finally {
+    setIsLoading(false);
+  }
+}, [profileData]);
+
+React.useEffect(() => {
+  getProfileData();
+}, []);
+
     return (
       <ProfileContainer>
-        <ProfilePicture src="http://github.com/srsouzaj.png" />
+        <ProfilePicture src={profileData.avatar_url} />
         <ProfileDetails>
           <header>
-            <h1>Jorge de Souza</h1>
-            <ExternalLink text="Github" href="http://github.com/srsouzaj" />
+            <h1>{profileData.name}</h1>
+            <ExternalLink text="Github" href={profileData.html_url} />
           </header>
-          <p>
-            WEB Developer | ReactJS | TS | Software Engineer at @recrutae
-          </p>
+          <p>{profileData.bio}</p>
           <ul>
             <li>
               <FontAwesomeIcon icon={faGithub} />
-              srsouzaj
+              {profileData.login}
             </li>
-            <li>
-              <FontAwesomeIcon icon={faBuilding} />
-              Recrutaê
-            </li>
+
+            {profileData.company && (
+              <li>
+                <FontAwesomeIcon icon={faBuilding} />
+                {profileData.company}
+              </li>
+            )}
+
             <li>
               <FontAwesomeIcon icon={faUserGroup} />
-              642 seguidores
+              {profileData.followers} seguidores
             </li>
           </ul>
         </ProfileDetails>
